79ebb51e26356ef34562e59af291b709ba1ae47b
Bruno Ferreira <bruno.ferreira@hosp.loc>
2026-01-14 08:09:22 -0300

    recuperando arquivos da branch recuperação

--- src/main/java/com/hsr/ativos/repositorys/ExtensionsRepo.java
+++ src/main/java/com/hsr/ativos/repositorys/ExtensionsRepo.java
@@ -3,9 +3,11 @@ package com.hsr.ativos.repositorys;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.hsr.ativos.models.Extensions;
 
+@Repository
 public interface ExtensionsRepo extends JpaRepository<Extensions, UUID> {
 
     
